8769fecc5f1dfc5c88e9a5d518d13160114ce069
Owen Worley <owen@owenworley.co.uk>
2018-01-12 12:19:31 +0000

    Invoke version script from regen task

--- scripts/set-version-number.sh
+++ scripts/set-version-number.sh
@@ -3,3 +3,4 @@ VERSION=$(grep -E 'OpenAPI spec version: [0-9x\.]+' api.ts | cut -d" " -f6)
 SED='s/\"version\": \".*\"/\"version\": \"'$VERSION'\"/g'
 sed -e "$SED" package.json > new_package.json
 mv new_package.json package.json
+echo Updated package to API version $VERSION
